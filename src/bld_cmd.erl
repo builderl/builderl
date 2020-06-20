b67e869f1d6f6adb8ba35461f67e4cb93cb905fe
Grzegorz Junka <list1@gjunka.com>
2020-06-20 17:25:09 +0000

    Fixes in executing commands

--- src/bld_cmd.erl
+++ src/bld_cmd.erl
@@ -33,22 +33,17 @@
 
 %%%-----------------------------------------------------------------------------
 %%% Copied from https://github.com/yoonka/yolf.git
--export([is_cmd/1, sh_cmd/1, cmd/1]).
+-export([is_cmd/1]).
 
 is_cmd(Bin) ->
-    case sh_cmd(<< <<"which ">>/binary, Bin/binary >>) of
+    case cmd(<< <<"which ">>/binary, Bin/binary >>) of
         {0, _} -> true;
         _ -> false
     end.
 
-sh_cmd(Bin) ->
-    EscBin = binary:replace(Bin, <<"\"">>, <<"\\\"">>, [global]),
-    Cmd = << <<"sh -c \"">>/binary, EscBin/binary, <<" 2>&1\"">>/binary >>,
-    cmd(Cmd).
-
 cmd(Command) ->
-    Args = [binary, in, eof, hide, exit_status, {line, 2048}],
-    Port = open_port({spawn, binary_to_list(Command)}, Args),
+    Args = [binary, stderr_to_stdout, exit_status, hide, {line, 2048}],
+    Port = open_port({spawn, Command}, Args),
     get_data(Port, {<<>>, []}).
 
 get_data(Port, {Line, Lines}) ->
@@ -58,16 +53,13 @@ get_data(Port, {Line, Lines}) ->
             get_data(Port, {<<>>, [NewLine | Lines]});
         {Port, {data, {noeol, Bytes}}} ->
             get_data(Port, {<<Line/binary, Bytes/binary>>, Lines});
-        {Port, eof} ->
-            {exit_code(Port), lists:reverse(Lines)}
+        {Port, {exit_status, Code}} ->
+            {Code, lists:reverse(Lines)};
+        Any ->
+            {2, [<<"Error: Unhandled message from port!">>,
+                binary:list_to_bin(io_lib:format(<<"~p">>, [Any]))
+            ]}
     end.
-
-exit_code(Port) ->
-    Port ! {self(), close},
-    receive {Port, closed} -> true end,
-    %% Remove EXIT message
-    receive {'EXIT',  Port,  _} -> ok after 1 -> ok end,
-    receive {Port, {exit_status, Code}} -> Code end.
 
 %%% Copied from https://github.com/yoonka/yolf.git
 %%%-----------------------------------------------------------------------------
@@ -75,7 +67,7 @@ exit_code(Port) ->
 execute_in(Path, Cmd) ->
     {ok, Cwd} = file:get_cwd(),
     file:set_cwd(filename:join(Cwd, Path)),
-    Res = sh_cmd(Cmd),
+    Res = cmd(Cmd),
     file:set_cwd(Cwd),
     Res.
 
@@ -93,13 +85,13 @@ git_cmd(Path, Cmd) ->
 git_status(Path) ->
     case filelib:is_dir(Path) of
         false -> false;
-        true -> sh_cmd(git_cmd(Path, <<"status --porcelain">>))
+        true -> cmd(git_cmd(Path, <<"status --porcelain">>))
     end.
 
 git_clone(Path, Tag, Clone) ->
     Cmd = << <<"git clone -q -b ">>/binary, Tag/binary, <<" ">>/binary,
              Clone/binary, <<" ">>/binary, Path/binary >>,
-    sh_cmd(Cmd).
+    cmd(Cmd).
 
 rm_rf(Path) ->
-    sh_cmd(<< <<"rm -rf \"">>/binary, Path/binary, <<"\"">>/binary >>).
+    cmd(<< <<"rm -rf \"">>/binary, Path/binary, <<"\"">>/binary >>).
